a4f5a1c049fa0bde3937ce066e9cbcb61e8920b4
Oleg <seyferseed@mail.ru>
2012-04-03 12:29:13 +0800

    new place for config

--- www/config/config.php
+++ www/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-
+//настройки подключения
 define (DB_NAME, 'course_wp');
 define (DB_HOST, 'localhost');
 define (DB_USER, 'course_user1');
--- www/models/startup.php
+++ www/models/startup.php
@@ -1,6 +1,6 @@
 <?php
 
-include "config.php";
+include $_SERVER['DOCUMENT_ROOT'] . "/config/config.php";
 
 function startup()
 {
